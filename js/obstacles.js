23544adfdb14a516be8d914ef78d17c99462000d
Francisco Martins <fmaraposo@gmail.com>
2020-10-26 21:52:11 +0000

    some changes

---
+++ js/obstacles.js
@@ -0,0 +1,12 @@
+class Obstacle {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    drawObstacles() {
+        const ObstacleImg = new Image();
+        const ObstacleImg2= new Image();
+
+    }
+}
