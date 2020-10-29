affb01baeb3e7719ce96a8da78e3c49907dda2e7
Francisco Martins <fmaraposo@gmail.com>
2020-10-29 20:54:49 +0000

    all set

--- js/game.js
+++ js/game.js
@@ -12,7 +12,7 @@ class Game {
         this.bonus = [];
         this.context= ctx;
         this.lives = '⭐⭐⭐⭐⭐';
-        this.kms = 3000;
+        this.kms = 100;
         this.gameIsRunning = false;
         this.gameMusic = true;
     }
